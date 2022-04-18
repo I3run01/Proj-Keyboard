1c90b3cc04e7319fee4b0f1999ab96288b01252d
I3run01 <brunnooa.v@gmail.com>
2022-04-18 17:33:13 -0300

    commit

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function text(value) {
-    document.querySelector('#text02').innerHTML = value
+    if(value == 'Eraser') text01 = text01.slice(0, length-1)
+    else  text01 += value
+    
+    document.querySelector('#text02').innerHTML = text01
 }
+
+let text01 = ''
